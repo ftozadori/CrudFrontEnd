84879bf62ff3ef0d7b85f62e2540d90890845747
ftozadori <ftozadori@gmail.com>
2025-05-13 22:18:42 -0300

    começo cadastro

---
+++ js/cadastro.js
@@ -0,0 +1,17 @@
+//para fazer o cadastro
+const botao = document.getElementById('btnCadastrar');
+const listaUsuariosCadastrados = [];
+
+// cadastrar
+botao.addEventListener('click', function (){
+    const objUsuario = {
+        usuario: document.getElementById('usuario').value.trim(),
+        senha: document.getElementById('senha').value
+    };
+    listaUsuariosCadastrados.push(objUsuario);
+    const listaJson = JSON.stringify
+    (listaUsuariosCadastrados);
+    localStorage.setItem('usuarios', listaJson);
+}
+
+);
