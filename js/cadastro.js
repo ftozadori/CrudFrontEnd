59952de188e06d9fd551ac736e2accd437a9fb58
Felipe Tozadori Silva <ftozadori@gmail.com>
2025-05-26 22:26:08 -0300

    cadastro att

--- js/cadastro.js
+++ js/cadastro.js
@@ -5,13 +5,35 @@ const listaUsuariosCadastrados = [];
 // cadastrar
 botao.addEventListener('click', function (){
     const objUsuario = {
-        usuario: document.getElementById('usuario').value.trim(),
+        usuario: document.getElementById('usuario').value(),
         senha: document.getElementById('senha').value
     };
     listaUsuariosCadastrados.push(objUsuario);
     const listaJson = JSON.stringify
     (listaUsuariosCadastrados);
     localStorage.setItem('usuarios', listaJson);
+    listar();
 }
 
 );
+
+//listar
+function listar(){
+    const listadeUsuarios = localStorage.JSON.parse(getItem("usuarios")) || [];
+    let tabela = document.getElementById('listaUsuarioCadastrados');
+    linha.innerHTML = "";
+    listadeUsuarios.forEach((objeto, index) => {
+        let linha = document.getElementById('tr');
+        linha.innerHTML = `
+            <td>${objeto.usuario}</td>
+            <td>${objeto.senha}</td>
+            <td>
+                <button>Editar</button>
+                <button>Remover</button>
+            </td>
+        `;
+        tabela.appendChild(linha);
+    });
+}
+
+listar();
